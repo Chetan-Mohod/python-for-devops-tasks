ec85e3675f76b7c77b67ff7e1213e7cd48c8201d
Chetan Mohod <55320509+Chetan-Mohod@users.noreply.github.com>
2026-03-16 16:12:45 +0530

    Update dict_ex.py

--- day-02/practice/dict_ex.py
+++ day-02/practice/dict_ex.py
@@ -1,19 +1,50 @@
+# info = {
+#     "name" : "Shubham Bhaiya", #str
+#     "city" : "Pune", #str
+#     "qualification": "Mtech",
+#     "age" : 29, # int
+#     "salary": 22.5, # float
+#     "married": True, # Bool
+#     "favourites" : ["teaching", "movies", 18]
+# }
+
+# print("I live in",info["city"])
+# print("I love ", info.get("favourite","Not Found"))
+
+# info.update({"channel": "TrainWithShubham"})
+
+# print(dir(info))
+
+# for key,value in info.items():
+#     print(key,value)
+
+
 info = {
-    "name" : "Shubham Bhaiya", #str
-    "city" : "Pune", #str
-    "qualification": "Mtech",
-    "age" : 29, # int
-    "salary": 22.5, # float
-    "married": True, # Bool
-    "favourites" : ["teaching", "movies", 18]
+    "name" : "Chetan Mohod",
+    "age" : 28,
+    "salary" : 1.9,
+    "city" : "Amravati",
+    "favourites" : ["Learning","Movies","Travel"],
+    "profession" : "Senior DevOps Engineer"
 }
 
-print("I live in",info["city"])
-print("I love ", info.get("favourite","Not Found"))
+print("My name is", info["name"])
+print("I have hobby of",info.get("favrouite","Not Found"))
 
-info.update({"channel": "TrainWithShubham"})
+print("I love to", info["favourites"][2])
+
+print(dir(info["favourites"]))
+
+print(info["favourites"].index.__doc__)
+
+info["favourites"].reverse()
+print("All hobbies:", info["favourites"])
+
+info.update({"Married" : False})
+
+print(info)
 
 print(dir(info))
 
 for key,value in info.items():
-    print(key,value)
+    print(f"{key}:{value}")
