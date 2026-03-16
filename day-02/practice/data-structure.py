e5bb9c48c7344253f845ddedfeb21e98a6bf853c
Chetan Mohod <55320509+Chetan-Mohod@users.noreply.github.com>
2026-03-16 16:11:33 +0530

    Create data-structure.py

---
+++ day-02/practice/data-structure.py
@@ -0,0 +1,22 @@
+#DataStructures 
+
+#********List*********
+
+Environment = ["dev","test","stg","prod"]
+print(len(Environment))
+print(Environment[3])
+#0,1,2,3 index positions
+
+#Environment.append(input("Enter new environment: "))
+Environment.append("uat")
+
+print(Environment)
+
+#Use pythonto teach you python by running below command.
+
+#First run below dir command with list object
+#print(dir(Environment))
+
+print(Environment.sort.__doc__)
+
+#********Dictionary*********
